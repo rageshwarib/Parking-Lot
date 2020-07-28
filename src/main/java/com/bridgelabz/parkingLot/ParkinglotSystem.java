4cf1881717c24b9aac74c168c8591c4a79248040
Rageshwari Bodke <ragu.bodke@gmail.com>
2020-07-28 17:46:53 +0530

    ADDED: Code that have test case and method to unpark the car and also check car is already unparked in parking lot system

--- src/main/java/com/bridgelabz/parkingLot/ParkinglotSystem.java
+++ src/main/java/com/bridgelabz/parkingLot/ParkinglotSystem.java
@@ -9,4 +9,12 @@ public class ParkinglotSystem {
         this.car = car;
         return true;
     }
+
+    public boolean unParkCar(Car car) {
+        if(this.car.equals(car)) {
+            this.car = null;
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/com/bridgelabz/parkingLot/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/parkingLot/ParkingLotTest.java
@@ -19,4 +19,18 @@ public class ParkingLotTest {
         boolean isParked = parkingLotSystem.parkCar(car);
         Assert.assertFalse(isParked);
     }
+    @Test
+    public void givenCar_WhenUnParked_ShouldReturnTrue() {
+        ParkinglotSystem parkingLotSystem = new ParkinglotSystem();
+        parkingLotSystem.parkCar(car);
+        boolean isUnParked = parkingLotSystem.unParkCar(car);
+        Assert.assertTrue(isUnParked);
+    }
+    @Test
+    public void givenCar_WhenAlreadyUnParked_ShouldReturnFalse() {
+        ParkinglotSystem parkingLotSystem = new ParkinglotSystem();
+        parkingLotSystem.parkCar(new Car());
+        boolean isUnParked = parkingLotSystem.unParkCar(car);
+        Assert.assertFalse(isUnParked);
+    }
 }
